30f9c021d6b897ce1de7aeff9c6a2e8303364ed5
Zoe-Hey <150931346+Zoe-Hey@users.noreply.github.com>
2024-04-09 14:36:44 +0200

    Declare variables as public to be accesed

--- Part1_POE/Program.cs
+++ Part1_POE/Program.cs
@@ -1,7 +1,16 @@
-﻿internal class Program
+﻿using System;
+
+internal class ProgramPart1
 {
+    public class Ingredients
+    {
+        public string name;
+        public double quantity;
+        public string unitsOfMeasurements;
+    }
+
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        
     }
 }
