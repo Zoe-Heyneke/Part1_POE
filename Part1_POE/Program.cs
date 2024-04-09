728d71bd449ee9da504b66138e60cebd288efdcf
Zoe-Hey <150931346+Zoe-Hey@users.noreply.github.com>
2024-04-09 14:56:51 +0200

    Ask user to enter recipe details: For loop for each ingredient to ask the user the name,quantity and units.

--- Part1_POE/Program.cs
+++ Part1_POE/Program.cs
@@ -17,6 +17,18 @@ internal class ProgramPart1
     public static void RecipeDetails()
     {
         Console.WriteLine("Enter the followig details for a single recipe");
-        Console.WriteLine("Enter the number of ingredient")
+        Console.WriteLine("Enter the number of ingredients: ");
+        int numIng = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("The number of ingredients chosen are: " + numIng);
+
+        for (int i = 0; i < numIng; i++) 
+        {
+            Console.WriteLine("Enter the name of the ingredient");
+            string nameIng = Console.ReadLine();
+            Console.WriteLine("Enter the quantity of the ingredient");
+            double quantityIng = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the unit of measurement of the ingredient");
+            string unitsMe = Console.ReadLine();
+        }
     }
 }
